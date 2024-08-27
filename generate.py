716727c94d8bb5f14b05d9b26c58bd979db2e79e
Tommaso Romano <34575973+tommasoromano@users.noreply.github.com>
2024-08-27 14:02:05 +0200

    results

--- generate.py
+++ generate.py
@@ -4,6 +4,7 @@ import os
 from nlp_synt_data import *
 from data.prompts import *
 from data.texts import *
+from sc import *
 
 # ollama run llama3:instruct
 # ollama run mistral:instruct
@@ -33,20 +34,40 @@ if __name__ == '__main__':
       print(d)
     exit()
 
-  # Generate synthetic data and run models
-  prompts = PromptGenerator.generate(PROMPTS_JOB_V0, PROMPTS_JOB_V0_KEYS)
+  SEED = ""
+  SEED = "seed-"
 
-  data = DataGenerator.generate(TEXT_v0, SUBS_JOBS_V0)
+  # Generate synthetic data and run models
+  prompts = PromptGenerator.generate(
+    PROMPTS_JOB_V0, 
+    # PROMPTS_JOB_V0_KEYS,
+    [['zsl',],],
+    )
+
+  # data = DataGenerator.generate(TEXT_v0, SUBS_JOBS_V0)
+  if SEED != "":
+    data = DataGenerator.generate(
+      TEXT_SEED_v0(), 
+      SUBS_JOBS_V0)
+  else:
+    data = sc_make_test_data()
+
   # model = 'llama3:instruct'
-  n_pass = 2
+  n_pass = 1
   model = 'gemma2'
   model = 'mistral'
   model = 'qwen2'
   model = 'llama3.1'
   model = 'phi3'
-  models = ['phi3','gemma2']
-  for model in models:
+  for model in [
+    'phi3',
+    # 'gemma2',
+    ]:
     print(model)
     model_f = lambda messages: ollama.chat(model=model, messages=messages)['message']['content']
-    ResponseGenerator.generate(f"results/{model}_split.csv", data, prompts,
-                               lambda prompt, text: PROMPTS_JOB_V0_F(prompt, text, model_f), n_pass=n_pass)
+    ResponseGenerator.generate(f"results/{model}_{SEED}split-long-v0.csv", 
+                               data, 
+                               prompts,
+                               lambda prompt, text: PROMPTS_JOB_V0_F(prompt, text, model_f), 
+                               n_pass=n_pass,
+                               )
